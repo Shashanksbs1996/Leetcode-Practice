1c7e54ee787d3b53c89d77627a1c68e04cac1e60
Shashank <ashashankshukla@gmail.com>
2022-04-24 02:32:57 +0530

    This is my Day one Angela yu Band Name Generator Project

---
+++ Day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name Generator")
+city =input("Which city do you live in ?\n")
+pet = input("Whats your pets name?\n")
+print("Your new Band name will be " + city +" " + pet)
