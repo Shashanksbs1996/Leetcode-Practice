3b9a15fe471ab4158c34f439f31cc10c0bc21d1e
Shashank <ashashankshukla@gmail.com>
2022-04-29 15:37:02 +0530

    Some Graphics Update in the game

--- Day4.py
+++ Day4.py
@@ -26,14 +26,43 @@ lose =('''
 
 
 
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
+
 
 userc =int(input("What do you choose? Type 0 for Rock,1 for paper or 2 for scissors\n"))
 if userc == 0:
-  print("You Choose Rock")
+  print(f"You Choose {rock}Rock")
 elif userc == 1:
-  print("You Choose Paper")
+  print(f"You Choose {paper}Paper")
 elif userc == 2:
-  print("You Chooose Scissor")
+  print(f"You Chooose {scissors}Scissor")
 else:
   print("Please Choose correctly")
   
@@ -41,11 +70,11 @@ else:
 comp=random.randint(0,2)
 
 if comp == 0:
-  print("Computer Choose Rock")
+  print(f"Computer Choose {rock} Rock")
 elif comp == 1:
-  print("Computer Choose Paper")
+  print(f"Computer Choose {paper}Paper")
 elif comp == 2:
-  print("Computer Chooose Scissor")
+  print(f"Computer Chooose {scissors}Scissor")
 
 if userc ==0:
   if comp == 2:
